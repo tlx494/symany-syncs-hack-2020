d1bd34dbb817bb6e96860c52b7c2a696ff99fd90
taron <taronling@gmail.com>
2020-08-28 23:28:41 +1000

    url validator added to link field

--- core/models.py
+++ core/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Document(models.Model):
     document_id = models.AutoField(primary_key=True)
-    title = models.TextField(blank=True)
-    link = models.TextField(blank=False)
+    title = models.TextField(blank=False)
+    link = models.URLField(blank=True, null=True)
     
