9d9311410ed755d6e9528de8efa61068c907982f
taron <taronling@gmail.com>
2020-08-28 22:04:40 +1000

    updated admin registered objects

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+import django.contrib.auth.admin
+from django.contrib.auth.models import User, Group
+from django.contrib import auth
+
+from .models import Document
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+admin.site.register(Document)
