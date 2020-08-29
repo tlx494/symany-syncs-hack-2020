4123492b503eb9b497d0c49d32240e570e00f9ef
Liang Pan <liangpan@Liangs-MacBook-Pro.local>
2020-08-30 00:03:03 +1000

    fixed checkbox

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,13 +1,16 @@
+isExtensionOn = true;
+console.log(isExtensionOn);
+
 window.onload = function () {
   var checkbox = document.querySelector("input[name=active-toggle]");
 
   checkbox.addEventListener( 'change', function() {
       if(this.checked) {
-        isExtensionOn = false;
-        console.log("off");
-      } else {
         isExtensionOn = true;
         console.log("on");
+      } else {
+        isExtensionOn = false;
+        console.log("off");
       }
   });
 };
