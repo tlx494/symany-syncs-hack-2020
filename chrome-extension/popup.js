ff27a09bfe8c1dcd77bc864a7f508f6608f351dc
Liang Pan <liangpan@Liangs-MacBook-Pro.local>
2020-08-29 13:51:34 +1000

    added UI of extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,20 +1,7 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+window.addEventListener("DOMContentLoaded", function () {
+  var form = document.getElementById("news");
 
-'use strict';
-
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+document.getElementById("your-id").addEventListener("click", function () {
+  form.submit();
 });
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+});
