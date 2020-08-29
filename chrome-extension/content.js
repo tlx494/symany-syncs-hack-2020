a905583683ec067f8a691e019af6ae060d00d258
Daniel O'Dea <dodea@atlassian.com>
2020-08-30 02:52:35 +1000

    added buttons and warning

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -9,6 +9,13 @@ window.onload = function () {
     console.log('Starting Symany...');
 }
 
+
+// chrome.contextMenus.create({
+//     title: "This is a test",
+//     contexts: ["selection"],
+//     onclick: searchSimilar()
+// })
+
 const verifyPost = async (title, link) => {
 
     let url = 'https://symanyapi.gq/check-post';
@@ -45,6 +52,7 @@ const searchForArticles = async () => {
 
     for (let i = 0; i < postLink.length; i++) {
         let postWindow = postLink[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        let postWindowChild = postLink[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
         let headline = postLink[i].getAttribute('aria-label');
         let link = postLink[i].getAttribute('href');
 
@@ -62,7 +70,43 @@ const searchForArticles = async () => {
             }
 
             if (!(verified['domain_is_dodgy'] || verified['title_is_dodgy'])) { // invert this later
-                postWindow.classList.add('dodgy');
+                postWindowChild.classList.add('dodgy');
+                let alertDiv = document.createElement('div');
+                let alertDiv2 = document.createElement('div');
+
+                let button1 = document.createElement('button');
+                button1.setAttribute('type', 'button');
+                button1.classList.add('btn', 'btn-primary', 'button1');
+                button1.innerHTML = 'Search Online';
+
+                button2.setAttribute('type', 'button');
+                button2.classList.add('btn', 'btn-primary', 'button2');
+                button2.innerHTML = 'See anyway';
+
+                verified['domain_is_dodgy'] = true; // delete this later
+                verified['warning_msg'] = 'This source is known for producing fake news' // delete this later
+
+                let alertText = '';
+                if (verified['domain_is_dodgy'] && verified['title_is_dodgy']) {
+                    alertText = `${verified['warning_msg']} and ${verified['title_warning']}`
+                } else if (verified['domain_is_dodgy']) {
+                    alertText = verified['warning_msg'];
+                } else if (verified['title_is_dodgy']) {
+                    alertText = verified['title_warning'];
+                }
+
+                let text = document.createTextNode('WARNING');
+                let text2 = document.createTextNode(alertText);
+
+                alertDiv.classList.add('alert-popup');
+                alertDiv2.classList.add('alert-popup-small');
+
+                alertDiv.appendChild(text);
+                alertDiv2.appendChild(text2);
+                postWindow.appendChild(alertDiv);
+                postWindow.appendChild(alertDiv2);
+                postWindow.appendChild(button1);
+                postWindow.appendChild(button2);
             }
 
             console.log('Updated posts:')
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,16 +1,43 @@
-isExtensionOn = true;
-console.log(isExtensionOn);
 
 window.onload = function () {
   var checkbox = document.querySelector("input[name=active-toggle]");
 
-  checkbox.addEventListener( 'change', function() {
-      if(this.checked) {
-        isExtensionOn = true;
-        console.log("on");
-      } else {
-        isExtensionOn = false;
-        console.log("off");
-      }
+  let extensionOn = false;
+  getStatus();
+
+
+  if (extensionOn == null) {
+    extensionOn = true;
+    setStatus(true);
+  } else {
+    if (checkbox.checked && !extensionOn) {
+      checkbox.click();
+    } else if (!checkbox.checked && extensionOn) {
+      checkbox.click();
+    }
+  }
+
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      extensionOn = true;
+      setStatus(true);
+      console.log("on");
+    } else {
+      extensionOn = false;
+      setStatus(false);
+      console.log("off");
+    }
   });
+
+  const setStatus = (isOn) => {
+    return chrome.storage.sync.set({ 'extensionOn': isOn });
+  }
+
+  const getStatus = () => {
+    chrome.storage.sync.get('extensionOn', function (res) {
+      extensionOn = res;
+    });
+  }
+
 };
+
