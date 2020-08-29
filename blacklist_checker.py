4339e836b1c65724856919ea06295b6b3faf1920
Daniel O'Dea <dodea@atlassian.com>
2020-08-29 17:13:26 +1000

    added post-searching functionality

--- blacklist_checker.py
+++ blacklist_checker.py
@@ -1,22 +1,25 @@
 import re
 
 
-def is_blacklisted(url):
-    domain = get_domain_from_url(url)
-    f = open('blacklist.txt', 'r')
-    for line in f:
-        if line.rstrip() == domain:
-            return True
-    return False
+class Blacklister:
+    def __init__(self):
+        self.blacklist_file = open('blacklist.txt', 'r')
 
+    def is_blacklisted(self, url):
+        domain = get_domain_from_url(url)
 
-def get_domain_from_url(url):
-    domain = ''
-    if url.startswith('http'):
-        match = re.search(r'(http[s]?:\/\/)([^\/\s]+)(\/.*)', url)
-        domain = match.group(2)
-    else:
-        match = re.search(r'([^\/\s]+)(\/.*)', url)
-        domain = match.group(1)
-    print('Found domain:', domain)
-    return domain
+        for line in self.blacklist_file:
+            if line.rstrip() == domain:
+                return True
+        return False
+
+    def get_domain_from_url(self, url):
+        domain = ''
+        if url.startswith('http'):
+            match = re.search(r'(http[s]?:\/\/)([^\/\s]+)(\/.*)', url)
+            domain = match.group(2)
+        else:
+            match = re.search(r'([^\/\s]+)(\/.*)', url)
+            domain = match.group(1)
+        print('Found domain:', domain)
+        return domain
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -5,30 +5,30 @@
 //     form.submit();
 // }
 
-const verifyPost = async (title, link) => {
+// const verifyPost = async (title, link) => {
 
-  let url = 'http://35.244.79.248/check-post'
-  let options = {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json;charset=UTF-8'
-    },
-    body: JSON.stringify({
-      title: title,
-      link: link
-    })
-  };
+//   let url = 'http://35.244.79.248/check-post'
+//   let options = {
+//     method: 'POST',
+//     mode: 'cors',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json;charset=UTF-8'
+//     },
+//     body: JSON.stringify({
+//       title: title,
+//       link: link
+//     })
+//   };
 
-  let response = await fetch(url, options);
-  let responseOK = response && response.ok;
-  if (responseOK) {
-    let data = await response.json();
+//   let response = await fetch(url, options);
+//   let responseOK = response && response.ok;
+//   if (responseOK) {
+//     let data = await response.json();
 
-    console.log(data);
-    // do something with data
-  }
-}
+//     console.log(data);
+//     // do something with data
+//   }
+// }
 
-verifyPost('This is a title', 'https://dodgywebsite.com');
+// verifyPost('This is a title', 'https://dodgywebsite.com');
