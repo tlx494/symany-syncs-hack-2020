d7b957229731c77ce76971c730ea3484f47b54d7
Symany <tlx494@gmail.com>
2020-08-29 09:15:57 +0000

    added certs

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def check_post():
 
 # run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
