a77ff4e57d7a8c3c546e067a5e5474eda98d2cea
charlie <charlie@onque.com.au>
2020-08-29 16:55:58 +1000

    added simple styling

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var ss = document.getElementById("customSwitch1");
 
 (ss).click(function() {
@@ -13,39 +12,3 @@ var ss = document.getElementById("customSwitch1");
     });
   }
 });
-=======
-// window.addEventListener("DOMContentLoaded", function () {
-//   var form = document.getElementById("news");
-
-//   document.getElementById("your-id").addEventListener("click", function () {
-//     form.submit();
-// }
-
-const verifyPost = async (title, link) => {
-
-  let url = 'http://35.244.79.248/check-post'
-  let options = {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json;charset=UTF-8'
-    },
-    body: JSON.stringify({
-      title: title,
-      link: link
-    })
-  };
-
-  let response = await fetch(url, options);
-  let responseOK = response && response.ok;
-  if (responseOK) {
-    let data = await response.json();
-
-    console.log(data);
-    // do something with data
-  }
-}
-
-verifyPost('This is a title', 'https://dodgywebsite.com');
->>>>>>> e0e7177ea14529e19701c06cdb87004db74fedbb
