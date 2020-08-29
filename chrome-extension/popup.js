6d626b49c3ef3b4b2bce2712a37c4120c9f6f1f9
Liang Pan <liangpan@Liangs-MacBook-Pro.local>
2020-08-29 22:45:10 +1000

    connected html and js

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,24 +1,15 @@
-// var ss = document.getElementById("customSwitch1");
+var isExtensionOn = true;
 
-// ($ss).click(function() {
-//   if(($ss).prop("checked")){
-//     chrome.runtime.sendMessage({isOn: "true"}, function(response) {
-//       console.log(response.farewell);
-//     });
-//   }
-//   else{
-//     chrome.runtime.sendMessage({isOn: "false"}, function(response) {
-//       console.log(response.state);
-//     });
-//   }
-// });
-console.log("hello");
-var checkbox = document.querySelector("input[name=checkbox]");
+window.onload = function () {
+  var checkbox = document.querySelector("input[name=checkbox]");
 
-checkbox.addEventListener( 'change', function() {
-    if(this.checked) {
-        console.log("hello");
-    } else {
-      console.log("hebyllo");
-    }
-});
+  checkbox.addEventListener( 'change', function() {
+      if(this.checked) {
+        isExtensionOn = false;
+        console.log("off");
+      } else {
+        isExtensionOn = true;
+        console.log("on");
+      }
+  });
+};
